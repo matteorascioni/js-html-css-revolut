1b23f243f7af4c6f39c3e81ea84b5b688b1f25b2
Matteo <Rascioni>
2020-10-28 17:39:42 +0100

    100%

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $( document ).ready(function() {
-    
+    // Refs 
+    // drop menu 
+    var dropLinks = $('.with-dropdown > a');
+    var dropMenu = $('.with-dropdown > .drop-js')
+
+     // Show / Hide Drop-js menu
+     dropLinks.click( function() {
+        var actualMenu = $(this).next('.drop-js');
+
+        dropMenu.not(actualMenu).hide();
+
+        actualMenu.toggle();
+    }); 
 });
